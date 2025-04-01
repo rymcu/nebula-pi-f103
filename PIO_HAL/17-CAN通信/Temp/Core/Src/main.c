748386371bafc40fb18aa39ef3ce0d43030378c7
Hugh-rymcu <hugh@rymcu.com>
2025-04-01 10:19:46 +0800

    .

--- PIO_HAL/17-CAN通信/Temp/Core/Src/main.c
+++ PIO_HAL/17-CAN通信/Temp/Core/Src/main.c
@@ -1,34 +1,39 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "stdio.h"//printf函数库
+#include "stdio.h" //printf函数�?
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-uint8_t rx_buff[100];  //接收缓存
-uint8_t rx_done = 0; //接收完成标志
-uint8_t rx_cnt = 0;//接收数据长度
+uint8_t rx_buff[100]; // 接收缓存
+uint8_t rx_done = 0;  // 接收完成标志
+uint8_t rx_cnt = 0;   // 接收数据长度
+// CAN全局变量
+CAN_TxHeaderTypeDef TxHeaderCAN;
+CAN_RxHeaderTypeDef RxHeaderCAN;
+uint8_t TxDataCAN[8], RxDataCAN[8];
+uint8_t CAN_Rx_Flag = 0; // CAN接收标志
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
@@ -42,6 +47,8 @@ uint8_t rx_cnt = 0;//接收数据长度
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+CAN_HandleTypeDef hcan;
+
 UART_HandleTypeDef huart1;
 
 /* USER CODE BEGIN PV */
@@ -52,8 +59,10 @@ UART_HandleTypeDef huart1;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART1_UART_Init(void);
+static void MX_CAN_Init(void);
 /* USER CODE BEGIN PFP */
-
+void filter_init(void);
+void CAN_Test(void);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -62,9 +71,9 @@ static void MX_USART1_UART_Init(void);
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
 
@@ -91,14 +100,11 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART1_UART_Init();
+  MX_CAN_Init();
   /* USER CODE BEGIN 2 */
-  //串口发送数据测试
-  unsigned char Sendbuf[] = "RYMCU nebula-pi usart1 test!\r\n";
-  HAL_UART_Transmit(&huart1,Sendbuf, sizeof(Sendbuf), HAL_MAX_DELAY); // 串口发送数据
   HAL_Delay(1000);
-  //printf打印数据测试
-  printf("hello,enjoy rymcu.com!\r\n");
-  HAL_Delay(1000);
+  filter_init();
+  CAN_Test();
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -108,33 +114,43 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-    if(rx_done == 1)//判读是否接收完成
+    // 处理CAN接收数据
+    if (CAN_Rx_Flag)
     {
-        rx_done = 0;//清除接收标志
-        //数据处理，打印接收长度、接收的数据
-        printf("length of rx data: %d!\r\n",rx_cnt);
-        for(int i = 0;i<rx_cnt;i++) printf("%c",rx_buff[i]);
-        printf("\r\n");
+      CAN_Rx_Flag = 0; // 清空CAN接收标志
+      printf("CAN receive data\r\n");
+      for (int i = 0; i < 8; i++)
+        printf(" 0x%02x", RxDataCAN[i]);
+      printf("\r\n");
+    }
+    if (rx_done == 1) // 判读是否接收完成
+    {
+      rx_done = 0; // 清除接收标志
+      // 数据处理，打印接收长度�?�接收的数据
+      printf("length of rx data: %d!\r\n", rx_cnt);
+      for (int i = 0; i < rx_cnt; i++)
+        printf("%c", rx_buff[i]);
+      printf("\r\n");
 
-        rx_cnt =0;//清除接收长度
-    } 
-    HAL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin); // 切换亮、灭状态，添加此语句防止优化   
+      rx_cnt = 0; // 清除接收长度
+    }
+    HAL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin); // 切换亮�?�灭状�?�，添加此语句防止优�?
   }
   /* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
@@ -148,9 +164,8 @@ void SystemClock_Config(void)
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
@@ -164,10 +179,52 @@ void SystemClock_Config(void)
 }
 
 /**
-  * @brief USART1 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief CAN Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_CAN_Init(void)
+{
+
+  /* USER CODE BEGIN CAN_Init 0 */
+
+  /* USER CODE END CAN_Init 0 */
+
+  /* USER CODE BEGIN CAN_Init 1 */
+
+  /* USER CODE END CAN_Init 1 */
+  hcan.Instance = CAN1;
+  hcan.Init.Prescaler = 9;
+  hcan.Init.Mode = CAN_MODE_LOOPBACK;
+  hcan.Init.SyncJumpWidth = CAN_SJW_1TQ;
+  hcan.Init.TimeSeg1 = CAN_BS1_5TQ;
+  hcan.Init.TimeSeg2 = CAN_BS2_2TQ;
+  hcan.Init.TimeTriggeredMode = DISABLE;
+  hcan.Init.AutoBusOff = DISABLE;
+  hcan.Init.AutoWakeUp = DISABLE;
+  hcan.Init.AutoRetransmission = DISABLE;
+  hcan.Init.ReceiveFifoLocked = DISABLE;
+  hcan.Init.TransmitFifoPriority = DISABLE;
+  if (HAL_CAN_Init(&hcan) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN CAN_Init 2 */
+    //启动CAN
+    if(HAL_CAN_Start(&hcan) != HAL_OK)
+    {
+        printf("CAN start Fail!\r\n");
+    }
+    //开启中断,FIFO 0接收消息中断
+    HAL_CAN_ActivateNotification(&hcan,CAN_IT_RX_FIFO0_MSG_PENDING);
+  /* USER CODE END CAN_Init 2 */
+}
+
+/**
+ * @brief USART1 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_USART1_UART_Init(void)
 {
 
@@ -191,22 +248,21 @@ static void MX_USART1_UART_Init(void)
     Error_Handler();
   }
   /* USER CODE BEGIN USART1_Init 2 */
-  //开启接收中断，空闲中断
-  __HAL_UART_ENABLE_IT(&huart1,UART_IT_IDLE|UART_IT_RXNE);
+  // �?启接收中断，空闲中断
+  __HAL_UART_ENABLE_IT(&huart1, UART_IT_IDLE | UART_IT_RXNE);
   /* USER CODE END USART1_Init 2 */
-
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+  /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
@@ -221,7 +277,7 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pins : K1_Pin K2_Pin */
-  GPIO_InitStruct.Pin = K1_Pin|K2_Pin;
+  GPIO_InitStruct.Pin = K1_Pin | K2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
@@ -246,28 +302,81 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
   HAL_GPIO_Init(LED1_GPIO_Port, &GPIO_InitStruct);
 
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-/* USER CODE END MX_GPIO_Init_2 */
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+  /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
 #ifdef __GNUC__
 #define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
 #else
-#define PUTCHAR_PROTOTYPE int fputc(int ch,FILE *f)
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
 #endif /* __GNUC__ */
-//重定向printf函数
+// 重定向printf函数
 PUTCHAR_PROTOTYPE
 {
-    HAL_UART_Transmit(&huart1,(uint8_t *)&ch,1,HAL_MAX_DELAY);//输出指向串口USART1
-    return ch;
+  HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, HAL_MAX_DELAY); // 输出指向串口USART1
+  return ch;
+}void filter_init(void)
+{
+    HAL_StatusTypeDef HAL_Status;
+    CAN_FilterTypeDef Filter0;
+    Filter0.FilterBank = 1;//滤波器编号
+    Filter0.FilterMode = CAN_FILTERMODE_IDMASK;
+    Filter0.FilterScale = CAN_FILTERSCALE_32BIT;
+    Filter0.FilterIdHigh = 0x00;
+    Filter0.FilterIdLow = 0x00;
+    Filter0.FilterMaskIdHigh = 0x00;
+    Filter0.FilterMaskIdLow = 0x00;
+    Filter0.FilterFIFOAssignment = CAN_FILTER_FIFO0;
+    Filter0.FilterActivation = CAN_FILTER_ENABLE;
+
+    HAL_Status = HAL_CAN_ConfigFilter(&hcan,&Filter0);
+    if(HAL_Status != HAL_OK)
+    {
+        printf("CAN Filter set Fail!code:%d\r\n",HAL_Status);
+        Error_Handler();
+    }
+}
+//重定义CAN接收中断回调函数
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
+{
+    HAL_StatusTypeDef HAL_Status;
+    if(hcan->Instance == CAN1)
+    {
+        HAL_Status = HAL_CAN_GetRxMessage(hcan,CAN_RX_FIFO0,&RxHeaderCAN,RxDataCAN);
+        if(HAL_Status == HAL_OK)
+        {
+            //处理接收数据
+            CAN_Rx_Flag = 1;
+        }
+    }
+}
+void CAN_Test(void)
+{
+    //发送数据CAN
+    TxHeaderCAN.ExtId = 0x1800F001;
+    TxHeaderCAN.DLC = 8;
+    TxHeaderCAN.IDE = CAN_ID_STD;
+    TxHeaderCAN.RTR = CAN_RTR_DATA;
+    TxHeaderCAN.StdId = 0x01;
+    TxHeaderCAN.TransmitGlobalTime = ENABLE;
+
+    uint32_t TxMailBox;
+    HAL_StatusTypeDef HAL_Status;
+    printf("\r\n\r\n------------------CAN test------------------\r\n\r\n");
+    for (int i = 0; i < 8; ++i) TxDataCAN[i] = i;
+    printf("CAN send data\r\n");
+    for (int i = 0; i < 8; ++i) printf(" 0x%02x",TxDataCAN[i]);
+    printf("\r\n");
+    HAL_CAN_AddTxMessage(&hcan,&TxHeaderCAN,TxDataCAN,&TxMailBox);
 }
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -279,14 +388,14 @@ void Error_Handler(void)
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
